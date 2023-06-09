12b39fecfa1b8c15597d73fe20fed52e4f7d4bb5
Karyah <mastrogiacomokaren@gmail.com>
2023-06-08 23:35:13 -0300

    colocando denovo o route

--- Global_Solution_Mobile/App.js
+++ Global_Solution_Mobile/App.js
@@ -3,14 +3,12 @@ import Routes from './routes'
 import MeuPerfil from './pages/MeuPerfil';
 import CadastroUsuario from './pages/CadastroUsuario';
 
-
 export default function App() {
   return (
     <>
-      <CadastroUsuario></CadastroUsuario>
-    {/* <NavigationContainer>
+     <NavigationContainer>
       <Routes/>
-    </NavigationContainer> */}
+    </NavigationContainer> 
     </>
   
   );
